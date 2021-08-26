0f407bb4dd2489dad9ed342a8a21d943418d75ca
Jordan <jordan.rouco-miguez@epitech.eu>
2021-08-26 13:58:01 +0200

    Update play.js

--- include/play.js
+++ include/play.js
@@ -21,13 +21,13 @@ module.exports = {
     const queue = message.client.queue.get(message.guild.id);
 
     if (!song) {
-      //if (STAY_TIME < 1000) {
+      if (STAY_TIME < 1000) {
         setTimeout(function () {
           if (queue.connection.dispatcher && message.guild.me.voice.channel) return;
           queue.channel.leave();
           queue.textChannel.send(i18n.__("play.leaveChannel"));
         }, STAY_TIME * 1000);
-      //}
+      }
       queue.textChannel.send(i18n.__("play.queueEnded")).catch(console.error);
       return message.client.queue.delete(message.guild.id);
     }
