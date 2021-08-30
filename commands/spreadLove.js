b3170849b3373c4908a48cf2793ca9c66fed0fcf
Jordan <jordan.rouco-miguez@epitech.eu>
2021-08-30 23:12:10 +0200

    Create spreadLove.js

---
+++ commands/spreadLove.js
@@ -0,0 +1,17 @@
+const { LOCALE } = require("../util/EvobotUtil");
+const i18n = require("i18n");
+
+i18n.setLocale(LOCALE);
+
+module.exports = {
+  name: "spreadlove",
+  aliases: ["sl"],
+  description: i18n.__("spreadlove.description"),
+  execute(message, args) {
+    if (!args.length)
+      return message
+        .reply(i18n.__mf("play.usageReply", { prefix: message.client.prefix }))
+        .catch(console.error);
+       return message.reply("@" + args + " sends you lot of ❤️❤️❤️❤️❤️");
+  }
+};
